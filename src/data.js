d31367a684131d63d1fd37b81d2faaf90cccf695
Alex Davies <alxdavies@googlemail.com>
2023-05-04 13:33:33 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,59 @@
+export const projects = [
+  {
+    title: "A White Workshop",
+    subtitle: "Wordpress",
+    description:
+      "Website created for carpenter and designer Alexander White",
+    image: "./awhitevideo.gif",
+    link: "http://www.awhiteworkshop.com/",
+  },
+  {
+    title: "Chestnuts Primary School logo",
+    subtitle: "Adobe Illustrator",
+    description:
+      "Modernisation of logo design, incorporating new colour scheme and illusrtrations inspired by nature",
+    image: "./chestnutslogo.png",
+  },
+  {
+    title: "Trezise",
+    subtitle: "Wordpress",
+    description:
+      "Website for Trezise holiday home in North Cornwall, including calendar and booking systems",
+    image: "./trezisevideo.gif",
+    link: "http://www.trezise-harlyn.co.uk",
+  },
+  {
+    title: "Guild Terminal",
+    subtitle: "Javascript",
+    description:
+      "Javascript funtionality for Wake The Tiger's Guild Terminal, allowing users to be assigned a Guild based on their quiz answers",
+    image: "./guildterminalvideo.gif",
+    link: "https://reactive.art/WTT/Guild/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Ruby",
+  "Ruby-on-Rails",
+  "Adobe Creative Cloud",
+  "Wordpress",
+];
