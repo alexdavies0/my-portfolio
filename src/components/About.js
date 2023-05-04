27cb094ea6aa76e88d719a19ffa72831d0efa387
Alex Davies <alxdavies@googlemail.com>
2023-05-04 21:32:47 +0100

    className removed from line break

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,8 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font font-eb-garamond sm:text-4xl text-3xl mb-4 font-medium text-black">
             Alex Davies
-            <br className="hidden lg:inline-block" />Designer and Web Developer
+            <br />
+            Designer and Web Developer
           </h1>
           <p className="mb-8 leading-relaxed">
             Full Stack and Front End developer. Visual designer.
